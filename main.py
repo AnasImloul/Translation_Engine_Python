85f65ac034cf080c466df2ccde9e53b01ceaf4bb
AnasImloul <76872415+AnasImloul@users.noreply.github.com>
2022-05-16 15:49:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
-import os
 from autocorrect import english_dictionary, correct
 from time import perf_counter_ns
-from syllables import closest_word
 
 words = "words"
 
 english = english_dictionary()
 
-sentence = "i hav such a botifol viow from my chambre"
+sentence = "i hev sush botifol viow frome my roome"
 
 start = perf_counter_ns()
 
 print(correct(sentence, english[words]))
 
 print((perf_counter_ns() - start)/1_000_000_000)
-
