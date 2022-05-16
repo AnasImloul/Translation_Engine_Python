f9b8c0aa9bdc8b5a9bfbc712b7a814ae7a2cb8ff
AnasImloul <76872415+AnasImloul@users.noreply.github.com>
2022-05-16 15:21:53 +0100

    Update syllables.py

--- syllables.py
+++ syllables.py
@@ -1,9 +1,9 @@
 a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z = 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 
 consonants = {'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'}
-composite_consonants = {"sh":{},"ch":{},"th":{d},"zz":{z,t+z},"ll":{l}}
+composite_consonants = {"sh":{"ch"},"ch":{"sh"},"th":{d},"zz":{z,t+z},"ll":{l}}
 
-similar_sound = {"":{e}, a:{e,i},b:{p},c:{s,k,q},d:{t},e:{a,i,y,""},f:{v},g:{j},h:{e,""},i:{e,a,y},j:{j},k:{c,q},l:{r},m:{n},n:{m},o:{u},p:{b},q:{c,k},r:{l},s:{c,z},t:{d,z},u:{y,o},v:{f,b},w:{v,o,u},x:{s,c,k},y:{i,e,u}, z:{s}}
+similar_sound = {"":{e}, a:{e,i},b:{p},c:{s,k,q},d:{t},e:{a,i,y,""},f:{v},g:{j},h:{e,""},i:{e,a,y},j:{j},k:{c,q},l:{r},m:{n},n:{m},o:{u},p:{b},q:{c,k},r:{l},s:{c,z},t:{d,z},u:{y,o},v:{f,b},w:{v},x:{s,c,k},y:{i,e,u}, z:{s}}
 
 vowels = {'a', 'e', 'i', 'o', 'u'}
 composite_vowels = {"au":{o},"ae":{e}, "ea":{e},"oe":{e},"ie":{e},"eau":{o},"eo":{"yo"}, "io":{"yo"}}
