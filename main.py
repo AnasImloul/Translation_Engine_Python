6f12508b3398cb1b44a44d1d52601c26b2f92066
AnasImloul <76872415+AnasImloul@users.noreply.github.com>
2022-05-22 12:48:47 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 from autocorrect import english_dictionary, correct
 from time import perf_counter_ns
 
-words = "words"
+words = "words.json"
 
 english = english_dictionary()
 
-sentence = "i hev sush botifol viow frome my roome"
+sentence = "i hab sach beotifl viow frum mi cambre"
 
 start = perf_counter_ns()
 
+
+
 print(correct(sentence, english[words]))
 
+
+
 print((perf_counter_ns() - start)/1_000_000_000)
+
