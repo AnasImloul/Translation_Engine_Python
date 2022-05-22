fd4aa96ee54cf2777f3eb7bc08c2bad911fa475f
AnasImloul <76872415+AnasImloul@users.noreply.github.com>
2022-05-22 19:13:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
 from autocorrect import english_dictionary, correct
-from time import perf_counter_ns
+from time import perf_counter
 
 words = "words.json"
 
 english = english_dictionary()
 
-sentence = "i hab sach beotifl viow frum mi cambre"
-
-start = perf_counter_ns()
-
-
-
-print(correct(sentence, english[words]))
+misspelled_sentence = "My trhee yera old duaghtre was trying to raost a marshmallow for teh first tiem Her fisrt adn seocnd attepmst ended in flmaes Both tiems I took the scochred marshmallow off of teh roatsign stikc and trhew it into the fire The tihrd tmie I helpde her a lot more adn together we achieved a perfectly tosaty golden borwn Ocne it was colo I handed her teh marshmallwo which seh promplty thrwe into teh fire No oen had tlod her seh was supposde to eat it Sbumitted by Luo Roses Parachute Cloroado Hree aer".lower()
+correct_sentence = "My three year old daughter was trying to roast a marshmallow for the first time Her first and second attempts ended in flames Both times I took the scorched marshmallow off of the roasting stick and threw it into the fire The third time I helped her a lot more and together we achieved a perfectly toasty golden brown Once it was cool I handed her the marshmallow which she promptly threw into the fire No one had told her she was supposed to eat it Submitted by Lou Roess Parachute Colorado Here are more".lower()
+start = perf_counter()
 
 
+corrected = correct(misspelled_sentence, english[words])
 
-print((perf_counter_ns() - start)/1_000_000_000)
 
+correct_count = 0
+
+for corrected_word,correct_word in zip(corrected.split(),correct_sentence.split()):
+    correct_count += corrected_word==correct_word
+
+print(f"correction precision : {round(correct_count/len(correct_sentence.split())*100,2)}%")
+
+
+print(f"Auto-corrected a text containing {len(correct_sentence.split())} words in {(perf_counter() - start)} seconds.")
