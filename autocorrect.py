854e1d9d7bfafe917b4935fc5500d1c7e8772663
AnasImloul <76872415+AnasImloul@users.noreply.github.com>
2022-05-22 12:49:11 +0100

    Update autocorrect.py

--- autocorrect.py
+++ autocorrect.py
@@ -1,25 +1,36 @@
 import os
-from syllables import closest_word
+from Syllables import closest_word as close1
+import json
 
-words = "words"
+words = "words.json"
 
 path = os.getcwd() + "/";
 
 ENGLISH = "languages/english/"
 
-english = {"words":set()}
+english = {words:set()}
+
 
 def english_dictionary():
-    with open(path + ENGLISH + words) as f:
-        english[words] = set(f.read().split("\n"))
+    with open(path + ENGLISH + words, 'r') as in_file:
+        try:
+            data = json.loads(in_file.read())
+        except json.JSONDecodeError:
+            data = dict()
+
+
+    english[words] = data
     return english
 
+
+def closest(word, dictionary):
+    closest1 = close1(word, dictionary)
+    return closest1
 
 def correct(sentence, dictionary):
     words = sentence.split()
 
-
     for i in range(len(words)):
-        words[i] = closest_word(words[i], dictionary)
+        words[i] = closest(words[i], dictionary)
 
     return " ".join(words)
